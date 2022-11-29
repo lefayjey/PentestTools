e4f0e1679830d659944b797de09e3fca2878f5f6
lefayjey <lefayjey@lefayjey.fr>
2022-11-29 15:05:11 +0100

    Update 29112022

---
+++ other/python3_https_server.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# python3 update of https://gist.github.com/dergachev/7028596
+# Create a basic certificate using openssl: 
+#     openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
+# Or to set CN, SAN and/or create a cert signed by your own root CA: https://thegreycorner.com/pentesting_stuff/writeups/selfsignedcert.html
+
+import http.server
+import ssl
+
+httpd = http.server.HTTPServer(('127.0.0.1', 443), http.server.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
+httpd.serve_forever()
